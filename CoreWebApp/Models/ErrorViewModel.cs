6283aedbe82f09be767597d51d909e6b234efb93
Prasad <kooldreamz@outlook.com>
2023-06-22 10:56:43 +0530

    Actual project (CoreWebApp) 3.1 deleted and 6.0 updated

--- CoreWebApp/Models/ErrorViewModel.cs
+++ CoreWebApp/Models/ErrorViewModel.cs
@@ -1,10 +1,8 @@
-using System;
-
 namespace CoreWebApp.Models
 {
     public class ErrorViewModel
     {
-        public string RequestId { get; set; }
+        public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
